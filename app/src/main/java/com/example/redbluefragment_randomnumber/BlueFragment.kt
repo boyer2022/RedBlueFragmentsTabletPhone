6f6445738068cdfe452f3eca8bc9c780ceb9e9f0
matt <emal>
2023-02-17 12:14:15 -0600

    Wk6 Video 5 complete. Added random text view

--- app/src/main/java/com/example/redbluefragment_randomnumber/BlueFragment.kt
+++ app/src/main/java/com/example/redbluefragment_randomnumber/BlueFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 /**
  * A simple [Fragment] subclass.
@@ -12,13 +14,25 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class BlueFragment : Fragment() {
-
+    private lateinit var displayRandomTextView: TextView
+    private val randomNumberViewModel:RandomNumberViewModel by lazy {
+        // lazy{} means - I'll describe how to create the RandomNumberViewModel, but the initialization won't run until
+        // that ViewModel is actually accessed
+        ViewModelProvider(requireActivity()).get(RandomNumberViewModel::class.java)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blue, container, false)
+        val view = inflater.inflate(R.layout.fragment_blue, container, false)
+        displayRandomTextView = view.findViewById(R.id.random_number_textview)
+        showRandomNumber()
+        return view
+    }
+    private fun showRandomNumber() {
+        val randomNumber = randomNumberViewModel.randomNumber
+        displayRandomTextView.text = randomNumber.toString()
     }
 
     companion object {
--- app/src/main/java/com/example/redbluefragment_randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/redbluefragment_randomnumber/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Need to listen for RedFragment
+        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_GENERATED, this) {
+                requestKey, bundle ->
+            // swap out red fragment for blue fragment
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainerView, BlueFragment.newInstance())
+                    .addToBackStack("BLUE")
+                    .commit()
+        }
+
+        // 1. add: Pause the red fragment, and add the blue fragment "on top"
+
+        // 2. replace: Replace the red fragment with the blue fragment <- red fragment is destroyed
+
+
+
     }
 }
